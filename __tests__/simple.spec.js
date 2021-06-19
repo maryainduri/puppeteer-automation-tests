dca8c75297ff8fb3c60c42dca759949ab61f3999
maryainduri <induri@trinnylondon.com>
2021-06-19 15:45:00 +0100

    added negative test

--- __tests__/simple.spec.js
+++ __tests__/simple.spec.js
@@ -5,9 +5,10 @@ let page;
 
 describe('should be able to login', async () => {
 
-    before(async () => {
+    beforeEach(async () => {
         browser = await puppeteer.launch({
             args: ['--no-sandbox'],
+            slowMo:20,
             executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
             headless: false,
         });
@@ -18,7 +19,7 @@ describe('should be able to login', async () => {
         });
     })
 
-    after(async () => {
+    afterEach(async () => {
         await browser.close()
     })
 
@@ -29,6 +30,17 @@ describe('should be able to login', async () => {
         await page.click('#login-button');
         await page.waitForSelector('#react-burger-menu-btn', {
             visible: true
+        });
+        console.log('User logged InIRL',await page.url())
+    });
+    it('should be able to login with invalid details', async () => {
+
+        await page.type('#user-name', 'standard_user')
+        await page.type('#password', 'secret_sauc')
+        await page.click('#login-button');
+        await page.waitForSelector('#react-burger-menu-btn', {
+            visible: true,
+            timeout: 10000
         });
         console.log('User logged InIRL',await page.url())
     });
