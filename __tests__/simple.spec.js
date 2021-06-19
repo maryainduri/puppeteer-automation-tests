36729cbee4970e185f8a800978affb5f97b6e3e9
maryainduri <induri@trinnylondon.com>
2021-06-19 15:17:25 +0100

    first commit

---
+++ __tests__/simple.spec.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+let browser;
+let page;
+describe('should be able to login', async () => {
+
+    before(async () => {
+        browser = await puppeteer.launch({
+            headless: false
+        });
+        page = await browser.newPage();
+        await page.goto('https://www.saucedemo.com/', {
+            waitUntil: 'networkidle2',
+        });
+    })
+
+    after(async () => {
+        await browser.close()
+    })
+
+    it('should be able to login with valid details', async () => {
+
+        await page.type('#user-name', 'standard_user')
+        await page.type('#password', 'secret_sauce')
+        await page.click('#login-button');
+        await page.waitForSelector('#react-burger-menu-btn',{
+            visible: true
+        })
+    });
+});
